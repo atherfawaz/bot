e854326684804395fa78c4acd7d8ad22b48f62af
Ather Fawaz <atherfawaz@gmail.com>
2024-01-18 23:53:11 +0400

    Add requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,14 @@
-import os
-from operator import itemgetter
-
 import streamlit as st
-from langchain import hub
 from langchain.agents import (
-    AgentExecutor,
     AgentType,
-    create_openai_tools_agent,
     initialize_agent,
-    load_tools,
 )
 from langchain.callbacks import StreamlitCallbackHandler
-from langchain.schema import format_document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.tools.retriever import create_retriever_tool
-from langchain_community.document_loaders import UnstructuredXMLLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents.base import Document
-from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import (
-    RunnableLambda,
-    RunnableParallel,
-    RunnablePassthrough,
-)
-from langchain_openai import ChatOpenAI, OpenAI, OpenAIEmbeddings
+from langchain_openai import OpenAI, OpenAIEmbeddings
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=30)
 docs = text_splitter.split_documents(documents=[Document(page_content="Ather")])
@@ -43,7 +26,7 @@ tools = [tool]
 
 st_callback = StreamlitCallbackHandler(st.container())
 
-llm = OpenAI(temperature=0, streaming=True)
+llm = OpenAI(temperature=0, streaming=True, model="gpt-4")
 agent = initialize_agent(
     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
