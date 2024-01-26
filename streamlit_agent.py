2112f6da998c817504303a18cd1ff3b91e34803c
Ather Fawaz <atherfawaz@gmail.com>
2024-01-26 14:04:21 +0400

    Revert

--- streamlit_agent.py
+++ streamlit_agent.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # from devtools import debug
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain.callbacks.base import BaseCallbackHandler
@@ -9,7 +9,8 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.memory import StreamlitChatMessageHistory
 from langchain.tools.retriever import create_retriever_tool
-from langchain_community.embeddings import HuggingFaceEmbeddings
+
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.pinecone import Pinecone
 from langchain_core.prompts import (
     ChatPromptTemplate,
@@ -17,9 +18,9 @@ from langchain_core.prompts import (
     SystemMessagePromptTemplate,
 )
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-load_dotenv()
+# load_dotenv()
 
 USER = "user"
 ASSISTANT = "ai"
@@ -48,7 +49,7 @@ class StreamHandler(BaseCallbackHandler):
 @st.cache_resource
 def get_llm() -> ChatOpenAI:
     return ChatOpenAI(
-        temperature=0.7,
+        temperature=0,
         model="gpt-4-1106-preview",
         streaming=True,
         verbose=True,
@@ -58,14 +59,13 @@ def get_llm() -> ChatOpenAI:
 
 @st.cache_resource
 def get_retriever():
+    embeddings = OpenAIEmbeddings()
     vectorstore = Pinecone.from_existing_index(
-        "catalog-v2",
-        HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2"),
+        "catalog-1536",
+        embeddings,
         "text",
     )
-    retriever = vectorstore.as_retriever(
-        search_type="mmr", search_kwargs={"k": 4, "score_threshold": 0.9}
-    )
+    retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 3})
     return retriever
 
 
@@ -74,8 +74,7 @@ def get_llm_agent():
     retriever_tool = create_retriever_tool(
         retriever,
         "search_electronics_and_home_appliances",
-        """Searches and returns information about products sold on noon.com. Query it when you need information about electronics and home appliances.
-        """,
+        "Searches and returns information about products sold on noon.com. Query it when you need information about electronics and home appliances.",
     )
     tools = []
     tools.append(retriever_tool)
@@ -87,22 +86,19 @@ def get_llm_agent():
             input_variables=[],
             template="""
             You are an ecommerce assistant of noon.com.
-            Your context is limited to products available on noon.com.
+            Your context is limited to the data passed to you. 
             Only answer questions related to products from electronics and home appliances.
             Prices are provided in the text for the products you receive, so find and return them from there.
-            Always return product URLs and link customers to the product page.
-            Always return image URLs and render images in markdown.
-            Present multiple products in a tabular format.
-            When given a price range in the search query, only show products that meet the criteria. If nothing meets it, say you don't have the products.
+            If you find product URLs use them to direct customer to that page.
+            If you find image URLs and render images in markdown.
+            When asked to compare products, compare them in a tabular format.
             When asked about delivery estimate or order status, direct to customer support.
             When asked about amazon or other websites, say that you are not aware of it.
             """,
         ),
     )
     agent = create_openai_tools_agent(llm, tools, agent_prompt)
-    agent_executor = AgentExecutor(
-        agent=agent, tools=tools, verbose=True, max_iterations=1
-    )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     agent_with_chat_history = RunnableWithMessageHistory(
         agent_executor,
         lambda session_id: history,
